a194a55a155d040acec92d9a9b4c8eeab34dbf49
sultrey <sulrey@Andrews-MacBook-Pro-4.local>
2020-04-26 14:51:38 -0700

    added some data, basic linear gradient, and random number function

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import { Header } from 'react-native-elements';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const HomeScreen = () => {
   return (
@@ -12,19 +13,42 @@ const HomeScreen = () => {
         centerComponent={{ text: 'CultureCards', style: { color: 'white' } }}
         containerStyle={{
         backgroundColor: '#FF6347',
-        justifyContent: 'space-around',
+        justifyContent: 'space-around'
         }}
       />
+      <View style={styles.cardStyle}>
+        <LinearGradient style={styles.gradientStyle} colors={['rgba(0,0,0,0.8)', 'transparent']}/>
+      </View>
     </View>
   )
 }
 
+// <Button title="Next Card" onPress={() => randomCard()}/>
+
+const randomCard = () => {
+  const cardNumber = Math.floor(Math.random() * 101);
+  return console.log(cardNumber);
+}
+
+const randomCardGenerator = () => {
+
+}
+
 const styles = StyleSheet.create({
-  titleStyle: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1
-  }
+    cardStyle: {
+      height: '83%',
+      width: '83%',
+      backgroundColor: '#FF6347',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    gradientStyle: {
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      top: 0,
+      height: '100%'
+    }
 });
 
 export default HomeScreen;
